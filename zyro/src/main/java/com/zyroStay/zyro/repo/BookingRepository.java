6e82b1b2ce3a13cf30d85b21819ea502cb1cac19
Panchashil <panchashilk7@gmail.com>
2025-12-25 12:56:01 +0530

    created repository for Users, Rooms, and Booking

---
+++ zyro/src/main/java/com/zyroStay/zyro/repo/BookingRepository.java
@@ -0,0 +1,14 @@
+package com.zyroStay.zyro.repo;
+
+import com.zyroStay.zyro.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookingRepository extends JpaRepository<Booking,Long> {
+
+    List<Booking> findByRoomId(Long roomId);
+    Optional<Booking> findByBookingConfirmationCode(String confirmationCode);
+    List<Booking> FinfByUserId(Long UserId);
+}
